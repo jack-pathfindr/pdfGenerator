d6ca8a0382e16c62cbacfc5ca53a31a9a3eef600
jack-pathfindr <jack.low@pathfindr.ai>
2025-10-27 14:37:41 +1100

    Update generatePdf.js

--- api/generatePdf.js
+++ api/generatePdf.js
@@ -20,7 +20,7 @@ export default async function handler(req) {
     // Your Browserless.io API token - REPLACE THIS!
     const BROWSERLESS_TOKEN = '2TJJeuZK0fBxxusb0b83b4d1af4ec3b4317b8feee48e7428d';
     
-    const browserlessUrl = `https://chrome.browserless.io/pdf?token=${2TJJeuZK0fBxxusb0b83b4d1af4ec3b4317b8feee48e7428d}`;
+    const browserlessUrl = `https://chrome.browserless.io/pdf?token=${BROWSERLESS_TOKEN}`;
     
     const response = await fetch(browserlessUrl, {
       method: 'POST',
